d8e8be46065d73f40abb6a0129c2e67b82110b45
sang <ghtkdrla321@naver.com>
2021-09-20 17:48:17 +0900

    app.js 처음시작 시 import React, {..없이 시작

---
+++ management/src/components/Customer.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import App from '../App';
+
+class Customer extends React.Component {
+    render() {
+        return(
+            <div>
+                <h2>홍길동</h2>
+                <p>999999</p>
+                <p>Male</p>
+                <p>univ</p>
+            </div>
+        )
+    }
+}
+
+export default App;
