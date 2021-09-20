016dd1b5bef5ab76deab665e0b410c18c24ec1f3
sang <ghtkdrla321@naver.com>
2021-09-20 18:02:33 +0900

    클론코딩 후 안돌아감

---
+++ managements/src/components/Customer.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+
+class Customer extends React.Component {
+    render() {
+        return ( 
+ 
+        <div>
+            <h2>홍</h2>
+            <p>99</p>
+            <p>Male</p>
+            <p>univ</p>
+        </div>
+        )
+    }
+}
+export default App;
